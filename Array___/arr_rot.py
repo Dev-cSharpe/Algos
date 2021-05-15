60f48f0730f92ae9ce7dd96c298dfaa0e7dd6df4
Dev-cSharpe <41975177+Dev-cSharpe@users.noreply.github.com>
2021-05-16 01:51:35 +0530

    not sure

--- Array___/arr_rot.py
+++ Array___/arr_rot.py
@@ -1,13 +1,4 @@
-arr=[1,2,3,4,5,6,7,8]
-
-
-
-key=arr[0]
-for i in range(0,len(arr)-1):
-    arr[i]=arr[i+1]
-
-arr[len(arr)-1]=key
-
-for i in arr:
-    print(i)
-
+first_name = 'Mukesh'
+last_name = 'kudi'
+message = f'{first_name} [{last_name}] is a Great Coder'
+print(message[0:])
